db316d091eba2880d7673e721896daf2e67473fc
Sebastián <jhoncagua603@gmail.com>
2021-10-18 18:56:09 -0500

    add the microservice and the configuration of eureka server

--- car-microservice/src/main/java/com/century/carmicroservice/CarMicroserviceApplication.java
+++ car-microservice/src/main/java/com/century/carmicroservice/CarMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.century.carmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CarMicroserviceApplication {
 
 	public static void main(String[] args) {
--- user-microservice/src/main/java/com/century/usermicroservice/UserMicroserviceApplication.java
+++ user-microservice/src/main/java/com/century/usermicroservice/UserMicroserviceApplication.java
@@ -2,11 +2,13 @@ package com.century.usermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //Necesario para que funcione Feign
 @EnableFeignClients
+@EnableEurekaClient
 public class UserMicroserviceApplication {
 
 	public static void main(String[] args) {
